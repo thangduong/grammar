98eb11607e30251e3b2d5d6b2e11a6827d582355
Thang Duong <thang@quantee.com>
2017-11-30 01:41:55 +0000

    ccnn determiner model

---
+++ exp/thduon/determiner.ccnn/eval_heldout.py
@@ -0,0 +1,89 @@
+from framework.utils.data.text_indexer import TextIndexer
+from word_classifier.data import ClassifierData
+from framework.evaluator import Evaluator
+import framework.utils.common as utils
+from time import time
+import numpy as np
+import os
+
+params = utils.load_param_file('output/determinerCCNNV3/params.py')
+#params = utils.load_param_file('params.py')
+
+vocab_file = os.path.join(utils.get_dict_value(params,'output_location'), 'vocab.pkl')
+ckpt = os.path.join(utils.get_dict_value(params,'output_location'),
+										utils.get_dict_value(params, 'model_name') + '.ckpt')
+
+e = Evaluator.load2(ckpt)
+i = TextIndexer.from_file(vocab_file)
+
+test_data = ClassifierData.get_monolingual_test(params=params)
+model_results = []
+
+timestr = str(int(time()))
+f = open(os.path.join(utils.get_dict_value(params,'output_location'),
+											'heldout_%s.txt'%timestr), 'w')
+fe = open(os.path.join(utils.get_dict_value(params,'output_location'),
+											'heldout_%s_err.txt'%timestr), 'w')
+fip = open(os.path.join(utils.get_dict_value(params,'output_location'),
+											'heldout_%s_err2.txt'%timestr), 'w')
+f.write('Exec Time\tModel Pick\tModel Score\tGround Truth\tSentence\n')
+fe.write('Exec Time\tModel Pick\tModel Score\tGround Truth\tSentence\n')
+no_right = [0,0,0,0]
+no_total = [0,0,0,0]
+no_total_model = [0,0,0,0]
+error_scenario = [[0,0,0,0],
+									[0, 0, 0, 0],
+									[0, 0, 0, 0],
+									[0, 0, 0, 0]]
+topn = 1
+last = 0
+for batch_no in range(1):
+	print("WORKING ON BATCH %s" % batch_no)
+	batch = test_data.next_batch(batch_size=200000)
+	for idx, (sentence, ground_truth, word) in enumerate(zip(batch['sentence'], batch['y'], batch['word'])):
+		_, indexed, _, _ = i.index_wordlist(sentence)
+		before_time = time()
+		r = e.eval({'sentence': [indexed], 'word': [word]}, {'sm_decision'})
+		after_time = time()
+		dt = after_time-before_time
+		pick = np.argpartition(r[0][0], -topn)[-topn:]
+#		pick = np.argmax(r[0][0])
+		pickpr = [r[0][0][idx] for idx in pick]
+		model_score = r[0][0]
+		x = [dt, pick, pickpr, ground_truth, sentence]
+		if ground_truth in pick:
+			no_right[ground_truth] += 1
+			fip.write('1 %s %s %s\n'%(dt,ground_truth,pickpr[0]))
+		else:
+			fip.write('0 %s %s %s\n'%(dt,ground_truth,pickpr[0]))
+			fe.write('%s\n' % x)
+		if (ground_truth == 1 and pick[0] == 2) or (ground_truth == 2 and pick[0] == 1):
+			print([idx, ground_truth, pick, pickpr])
+			print(sentence)
+		error_scenario[ground_truth][pick[0]] += 1
+		no_total[ground_truth] += 1
+		no_total_model[pick[0]] += 1
+		model_results.append(x)
+		f.write('%s\n'%x)
+		if idx-last > 10000:
+			print("recall = %s" % [x / y for x, y in zip(no_right, no_total)])
+			print("precision = %s" % [x / y for x, y in zip(no_right, no_total_model)])
+			print(no_right)
+			print(no_total)
+			print(no_total_model)
+			print(error_scenario)
+			last =idx
+
+#for x in model_results:
+#	print(x)
+print("recall = %s"%[x/y for x,y in zip(no_right,no_total)])
+print("precision = %s"%[x/y for x,y in zip(no_right, no_total_model)])
+print(no_right)
+print(no_total)
+print(no_total_model)
+
+print(error_scenario)
+#		f.write('%0.8f\t%0.8f\t%s\t%s\n'%(after_time-before_time,model_score,ground_truth,sentence))
+f.close()
+fe.close()
+fip.close()
---
+++ exp/thduon/determiner.ccnn/model.py
@@ -0,0 +1,70 @@
+import tensorflow as tf
+import framework.subgraph.nlp as nlp
+import framework.utils.common as utils
+import framework.subgraph.mlp as mlp
+import framework.subgraph.misc as misc
+import framework.subgraph.core as core
+
+def sentence_encoder(emb_sentence, word_emb, params, name='encoded_sentence'):
+	"""
+	@param emb_sentence:
+	@param params:
+	@return:
+	"""
+	conv_num_features = utils.get_dict_value(params, 'conv_num_features')
+	conv_widths = utils.get_dict_value(params, 'conv_widths')
+	conv_keep_probs = utils.get_dict_value(params, 'conv_keep_probs')
+	mlp_config = utils.get_dict_value(params, 'mlp_config')
+	bipass_conv = utils.get_dict_value(params, 'bipass_conv')
+	mlp_activations = utils.get_dict_value(params, 'mlp_activations')
+	mlp_dropout_keep_probs = utils.get_dict_value(params, 'mlp_keep_probs')
+	use_no_conv_path = utils.get_dict_value(params, 'use_no_conv_path')
+	if bipass_conv:
+		conv_group = [emb_sentence]
+	else:
+		if use_no_conv_path:
+			conv_group = [emb_sentence]
+		else:
+			conv_group = []
+		for i, (conv_num_feature, conv_width) in enumerate(zip(conv_num_features, conv_widths)):
+			conv_out = nlp.conv1d_array(emb_sentence, conv_num_feature, conv_width,name='conv%s'%(str(i)), w_wds=0.000, b_wds=0.000, keep_probs=conv_keep_probs)
+			conv_group.append(conv_out)
+	conv_group.append(word_emb)
+	conv_out, _ = misc.concat(conv_group)
+	mlp_out, _ = mlp.fully_connected_network(conv_out, mlp_config, layer_activations=mlp_activations, dropout_keep_probs=mlp_dropout_keep_probs)
+	return [tf.identity(mlp_out[0], name=name)], {}
+
+
+def inference(params):
+	embedding_size = params['embedding_size']
+	vocab_size = params['vocab_size']
+	sentence_len = params['num_words_before'] + params['num_words_after']
+	embedding_wd = utils.get_dict_value(params, 'embedding_wd')
+	embedding_device = utils.get_dict_value(params, 'embedding_device')
+	embedding_initializer = utils.get_dict_value(params, 'embedding_initializer')
+	embedding_keep_prob = utils.get_dict_value(params, 'embedding_keep_prob')
+	word_embedding_size = utils.get_dict_value(params, 'word_embedding_size', embedding_size)
+	word_len = params['word_len']
+
+	print("USING EMBEDDING DEVICE %s" %embedding_device)
+	if embedding_device is not None:
+		with tf.device(embedding_device):
+			embedding_matrix = nlp.variable_with_weight_decay('embedding_matrix',
+																												[vocab_size, embedding_size],
+																												initializer=embedding_initializer, wd=embedding_wd)
+	else:
+		embedding_matrix = nlp.variable_with_weight_decay('embedding_matrix', [vocab_size, embedding_size],
+																											initializer=embedding_initializer, wd=embedding_wd)
+	word_embedding_matrix = nlp.variable_with_weight_decay('word_embedding_matrix', [128, word_embedding_size],
+																										initializer=embedding_initializer, wd=embedding_wd)
+
+	if embedding_keep_prob is not None and embedding_keep_prob < 1.0:
+		[embedding_matrix],_ = core.dropout([embedding_matrix], [embedding_keep_prob])
+	input_sentence = tf.placeholder(tf.int32, [None, sentence_len], 'sentence')
+	input_word = tf.placeholder(tf.int32, [None, word_len], 'word')
+	emb_sentence = tf.nn.embedding_lookup(embedding_matrix, input_sentence, 'emb_sentence')
+	emb_word = tf.nn.embedding_lookup(word_embedding_matrix, input_word, 'emb_word')
+#	emb_sentence = tf.concat([emb_sentence, emb_word], axis=1)
+	enc_sentence, _ = sentence_encoder(emb_sentence, emb_word, params)
+
+	return enc_sentence, None
