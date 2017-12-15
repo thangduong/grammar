41e21303617fbfb2f136ee4f75c66d4e1297343f
Thang Duong <thduon@microsoft.com>
2017-12-15 14:35:26 -0800

    made tokenizer work for windows

--- exp/thduon/tokenizer/bin/tokenizer.py
+++ exp/thduon/tokenizer/bin/tokenizer.py
@@ -4,43 +4,35 @@ import os
 script_dir = os.path.dirname(os.path.realpath(__file__))
 
 if os.name=='nt':
-	tok_so = cdll.LoadLibrary(os.path.join(script_dir,'tokenizer_dll.dll'))
-	tokenizer = tok_so.LoadTokenizer()
+	tok_so = cdll.LoadLibrary(os.path.join(script_dir,'tokenizer.dll'))
 else:
 	tok_so = cdll.LoadLibrary(os.path.join(script_dir,'tokenizer_so.so'))
-	tokenizer = tok_so.LoadTokenizer()
-"""
-input_string = c_char_p("hello this is a 500 test!".encode('utf-8'))
-result = tok_so.TokenizeString (tokenizer, input_string, True)
+tok_so.LoadTokenizer.argtypes = []
+tok_so.LoadTokenizer.restype = c_void_p
+tok_so.TokenizeString.argtypes = [c_void_p, c_char_p, c_bool]
+tok_so.TokenizeString.restype = c_void_p
 tok_so.NextToken.argtypes = [c_void_p, POINTER(c_int), POINTER(c_int), POINTER(c_int)]
-start = c_int()
-len = c_int()
-type = c_int()
-done = False
-while (not done):
-	ctoken = tok_so.NextToken(result, byref(start), byref(len), byref(type))
-	done = ctoken == 0
-	if not done:
-		token = [c_char_p(ctoken).value.decode('utf-8'), start.value, len.value, type.value]
-		print(token)
-#tok_so.FreeTokenizedResult(result)
-#tok_so.UnloadTokenizer(tokenizer)
-"""
-def tokenize_string(string, translit=True):
-	input_string = c_char_p(string.encode('utf-8'))
-	tokenizer_result = tok_so.TokenizeString(tokenizer, input_string, translit)
-	tok_so.NextToken.argtypes = [c_void_p, POINTER(c_int), POINTER(c_int), POINTER(c_int)]
-	start = c_int()
-	len = c_int()
-	type = c_int()
-	done = False
-	result = []
-	while (not done):
-		ctoken = tok_so.NextToken(tokenizer_result, byref(start), byref(len), byref(type))
-		done = ctoken == 0
-		if not done:
-			token = [c_char_p(ctoken).value.decode('utf-8'), start.value, len.value, type.value]
-			result.append(token)
-	return result
+tok_so.NextToken.restype = c_char_p
+tok_so.FreeTokenizedResult.argtypes = [c_void_p]
 
-#print(tokenize_string("this is a test 510-847-7898 i am 50 years old from the 90s"))
+class Tokenizer:
+	def __init__(self):
+		self._tokenizer = tok_so.LoadTokenizer()
+
+	def tokenize(self, string, translit=True):
+		input_string = c_char_p(string.encode('utf-8'))
+		tokenizer_result = tok_so.TokenizeString(self._tokenizer, input_string, translit)
+		start = c_int()
+		len = c_int()
+		type = c_int()
+		done = False
+		result = []
+		while (not done):
+			ctoken = tok_so.NextToken(tokenizer_result, byref(start), byref(len), byref(type))
+			if ctoken is not None:
+				token = [c_char_p(ctoken).value.decode('utf-8'), start.value, len.value, type.value]
+				result.append(token)
+			else:
+				done = True
+		tok_so.FreeTokenizedResult(tokenizer_result)
+		return result
--- exp/thduon/tokenizer/tokenizer/Tokenizer_wrapper.h
+++ exp/thduon/tokenizer/tokenizer/Tokenizer_wrapper.h
@@ -1,9 +1,8 @@
 #pragma once
-#ifndef _WIN32
-#define __stdcall
-#endif
-using PFN_LoadTokenizer = void*(__stdcall *)();
-using PFN_UnloadTokenizer = void(__stdcall*)(void* tokenizer);
-using PFN_TokenizeString = void*(__stdcall *)(void* tokenizer, const char* czInput, bool translit);
-using PFN_NextToken = void*(__stdcall *)(void* result, int* start, int* len, int* type);
-using PFN_FreeTokenizedResult = void(__stdcall*)(void* result);
+#define CALL_CONV
+
+using PFN_LoadTokenizer = void*(CALL_CONV *)();
+using PFN_UnloadTokenizer = void(CALL_CONV*)(void* tokenizer);
+using PFN_TokenizeString = void*(CALL_CONV *)(void* tokenizer, const char* czInput, bool translit);
+using PFN_NextToken = void*(CALL_CONV *)(void* result, int* start, int* len, int* type);
+using PFN_FreeTokenizedResult = void(CALL_CONV *)(void* result);
