dfc476900d7d88c8d5ee058db181677f584391cb
Thang Duong <thduon@microsoft.com>
2017-12-18 14:05:22 -0800

    c# interop

---
+++ exp/thduon/tokenizer/tokenizercsTest/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TokenizerCS;
+
+namespace tokenizercsTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tokenizer tok = new Tokenizer();
+            var result = tok.Tokenize("F.D.R. was a great president in the 1930s.");
+            foreach (var token in result)
+            {
+                System.Console.WriteLine(token.token);
+            }
+        }
+    }
+}
