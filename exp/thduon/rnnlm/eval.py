0395db4d7bdd3cfb1494eeb9de8088b7fa744a74
Thang Duong <thang@thanggpu.kxg3qnfci5iehlj0ssy0vzbsoc.xx.internal.cloudapp.net>
2017-12-21 11:03:36 +0000

    fixed an embarrassing bug

--- exp/thduon/rnnlm/eval.py
+++ exp/thduon/rnnlm/eval.py
@@ -8,7 +8,7 @@ import pickle
 import math
 run_server = False
 
-params = utils.load_param_file('output/rnnlmV5/params.py')
+params = utils.load_param_file('output/rnnlmV2/params.py')
 
 vocab_file = os.path.join(utils.get_dict_value(params,'output_location'), 'vocab.pkl')
 ckpt = os.path.join(utils.get_dict_value(params,'output_location'),
@@ -23,9 +23,9 @@ num_after = utils.get_dict_value(params, "num_words_after")
 pad_tok = utils.get_dict_value(params, "pad_tok", '<pad>')
 
 #sentences = ['<s> The apple , which is rotten , is not edible .']
-sentences = ['<s> I ate a pair of orange .'
-							,'<s> I ate a pear of orange .'
-							,'<s> I ate a pair of oranges .'
+sentences = ["I ate a pair of orange ."
+							,'I ate a pear of orange .'
+							,'I ate a pair of oranges .'
 						 ]
 cell_size = params['cell_size']
 num_steps = params['num_steps']
@@ -37,27 +37,37 @@ if os.path.exists('sent_list.pkl'):# and False:
 all_sentence_prob = {}
 for sentence in sentences:
 	orig_sentence = sentence
+	sentence = "<s> " + sentence
 	sentence = sentence.lower()
 	state = np.zeros([num_layers, 2, 1, cell_size])
 	tokens = sentence.split()
 	sentence_len = len(tokens)
+#	print(tokens)
+#	print(sentence_len)
 	sentence_prob = 1.0
 	before = time.time()
 	if (len(tokens)%num_steps) > 0:
 		tokens += ['.'] * (num_steps-(len(tokens)%num_steps))
 	_,indexed,num_unk,unk_words = i.index_wordlist(tokens)
+#	print(indexed)
 	for toki in range((int(len(indexed)/num_steps))):
 		r = e.eval({'x': [indexed[(toki*num_steps):(toki+1)*num_steps]],
 								'state': state}, ['output_logits_sm', 'final_state'])
 		state = r[1]
 		for j in range(num_steps):
-			#print(r[0][0][j][indexed[toki*20+j]])
-			sentence_prob *= r[0][0][j][indexed[toki*20+j]]
-			if toki*20+j > sentence_len:
+			if toki*20+j >= sentence_len-1:
 				break
+			sm = np.argmax(r[0][0][j])
+			#print(r[0][0][j][indexed[toki*20+j]])
+			nw = indexed[toki*20+j+1]
+			wprob = r[0][0][j][nw]
+			sentence_prob *= wprob
+#			print("%s - %s"%(i._index_to_word[nw],r[0][0][j][nw]))
+#			print("%s: %s: %s, %s, %s"%(sentence_prob,wprob,tokens[toki*20+j],i._index_to_word[sm],r[0][0][j][sm]))
 	diff = time.time() - before
 	print("execute time = %s, unk_count = %s %s" % (diff, num_unk, unk_words))
-	print("%s:%s:%s"%(sentence_prob, math.log(sentence_prob), sentence))
+#	print("%s:%s:%s"%(sentence_prob, math.log(sentence_prob), sentence))
+	print("%s:%s"%(sentence_prob,  sentence))
 	all_sentence_prob[orig_sentence] = (sentence_prob)
 
 with open('sent_prob.pkl', 'wb') as f:
