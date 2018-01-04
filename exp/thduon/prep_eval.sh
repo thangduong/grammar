ebe5ab3a6ec055dd262b2bbb044354e4d7ce2e39
Thang Duong <thduon@microsoft.com>
2018-01-04 00:13:41 -0800

    for sourcing from outside

--- exp/thduon/prep_eval.sh
+++ exp/thduon/prep_eval.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
 source /mnt/work/env-tf/bin/activate
-export PYTHONPATH=$(readlink --canonicalize ../../dlframework)
+export PYTHONPATH=$PYTHONPATH:$(readlink -f $(dirname ${BASH_SOURCE[@]})/../../dlframework)
 export CUDA_VISIBLE_DEVICES=
+pushd . > /dev/null
