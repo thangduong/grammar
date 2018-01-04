5cb4490b1a1b74f5b93f1ee8c28244d31bc8dccb
Thang Duong <thduon@microsoft.com>
2018-01-04 00:18:26 -0800

    modified so that if ./prep.sh <gpu>, if <gpu> not present, then set CUDA_VISIBLE_DEVICES to blank.  changed name to prep.

--- exp/thduon/prep.sh
+++ exp/thduon/prep.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 source /mnt/work/env-tf/bin/activate
 export PYTHONPATH=$PYTHONPATH:$(readlink -f $(dirname ${BASH_SOURCE[@]})/../../dlframework)
-export CUDA_VISIBLE_DEVICES=
+if [ "$#" -eq 1 ]; then
+	export CUDA_VISIBLE_DEVICES=$1
+else
+	export CUDA_VISIBLE_DEVICES=
+fi
 pushd . > /dev/null
